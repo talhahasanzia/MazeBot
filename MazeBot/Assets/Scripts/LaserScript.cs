63bab4d9df82602c4f929f6684427c0c49422ca6
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-08-22 17:08:28 +0500

    Laser hit disability

--- MazeBot/Assets/Scripts/LaserScript.cs
+++ MazeBot/Assets/Scripts/LaserScript.cs
@@ -15,74 +15,102 @@ public class LaserScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
+        
+
         Debug.Log("Enemy hit: " + EnemyHit);
         if (Input.GetButtonDown("Fire1"))
         {
-            StopCoroutine(LaserCoroutine());
 
-            StartCoroutine(LaserCoroutine());
-        
+            StopCoroutine(Laser());
+            StartCoroutine(Laser());
         
         }
-        if(EnemyHit)
+        if (EnemyHit)
         {
             InitializeJammer();
-            
 
+
+        }
+        else
+        {
+
+            DisableJammer();
+        
         }
 
 	
 	}
 
     void InitializeJammer()
-    { 
-    
-    
-    
-    
+    {
+
+
+        GameObject hitObject = rayHit.collider.gameObject;
+
+        hitObject.GetComponent<EnemyBehaviour>().enabled = false;
+
+
+        StartCoroutine(DisableJammer());
     
     
     
     }
-    IEnumerator LaserCoroutine()
+
+
+    IEnumerator DisableJammer()
     {
+        yield return new WaitForSeconds(3);
+
+
+
+        GameObject hitObject = rayHit.collider.gameObject;
+
+            hitObject.GetComponent<EnemyBehaviour>().enabled = true;
+        
+    
+    }
+
+
+    IEnumerator Laser()
+    {
+
+
 
         line.enabled = true;
 
         while (Input.GetButton("Fire1"))
         {
-           
 
-            
+
+
             Ray rayObj = new Ray(transform.position, transform.forward);
-            
-            
+
+
             line.SetPosition(0, rayObj.origin);
-            
-            
-            if(Physics.Raycast(rayObj,out rayHit))
+
+
+            if (Physics.Raycast(rayObj, out rayHit))
             {
-                
+
                 line.SetPosition(1, rayHit.point);
-               // Debug.Log(rayHit.collider.tag);
+                // Debug.Log(rayHit.collider.tag);
 
 
                 if (rayHit.collider.tag == "Enemy")
                     EnemyHit = true;
 
             }
-            
+
             line.SetPosition(1, rayObj.GetPoint(100));
 
+
             yield return null;
-        
-        
+
         }
 
         line.enabled = false;
-
-
-       
-        
+    
     }
+    
 }
