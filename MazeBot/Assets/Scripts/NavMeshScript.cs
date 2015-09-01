996167edfa501b72981c6b527ea4c6219c932d72
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-09-01 17:41:38 +0500

    Implemented Navmesh path to enemy model

--- MazeBot/Assets/Scripts/NavMeshScript.cs
+++ MazeBot/Assets/Scripts/NavMeshScript.cs
@@ -5,21 +5,30 @@ public class NavMeshScript : MonoBehaviour {
 
 
     public Transform Target;
+   public Transform StartPos;
+    public Transform Finish;
     NavMeshAgent NavAgent;
 
 	// Use this for initialization
 	void Start () {
         NavAgent = this.GetComponent<NavMeshAgent>();
-
+        NavAgent.SetDestination(Finish.position);
+        
        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-       
+
+        if ((transform.position - Finish.position).magnitude<5)
+            NavAgent.SetDestination(StartPos.position);
 
 
-        if(NavAgent.isActiveAndEnabled==true)
-        NavAgent.destination = Target.position;
+        if ((transform.position - StartPos.position).magnitude<5)
+            NavAgent.SetDestination(Finish.position);
+
+        
+        //if(NavAgent.isActiveAndEnabled==true)
+        //NavAgent.destination = Target.position;
 	}
 }
